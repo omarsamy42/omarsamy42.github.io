cc2c703dcf06d27413dc2de804afcfd2847d375a
hanahesham1786-dotcom <hanahesham1786@gmail.com>
2026-05-03 19:10:28 +0300

    Add files via upload

---
+++ omar/IT PROJECT1/java.js
@@ -0,0 +1,20 @@
+
+
+let activeElement= document.querySelector('.cart');
+function open_close_cart(){
+    activeElement.classList.toggle('active')
+}
+document.body.appendChild(activeElement);
+// var swiper = new Swiper(".slide-swp", {
+//       pagination: {
+//         el: ".swiper-pagination",
+//         dynamicBullests: true,
+//         clickable:true,
+//       },
+//       autoplay:{
+//       delay:2500,
+//       },
+//       loop:true
+//     });
+    
+
