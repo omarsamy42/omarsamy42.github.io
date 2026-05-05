5d6d6c408ef3111a7af93525881135502092a782
hanahesham1786-dotcom <hanahesham1786@gmail.com>
2026-05-05 06:07:08 +0300

    Update java.js

--- omar/IT PROJECT1/java.js
+++ omar/IT PROJECT1/java.js
@@ -1,11 +1,10 @@
-
 function open_close_cart(){
 let activeElement= document.querySelector('.cart');
 if(activeElement){
     activeElement.classList.toggle('active')
 }
 }
-
+try{
 var swiper = new Swiper(".mySwiper", {
       pagination: {
         el: ".swiper-pagination",
@@ -16,7 +15,42 @@ var swiper = new Swiper(".mySwiper", {
       delay:2500,
       },
       loop:true
-    });
+    } );
+   } catch(e) {
+    console.log("Swiper not loaded");
+}
+
+
+
+let darkmode =localStorage.getItem('darkmode');
+const themeSwitch =document.getElementById('theme-switch');
+
+const enableDarkmode = ()=>{
+
+  document.body.classList.add('dark');
+  localStorage.setItem('darkmode','active');
+
+}
+const disableDarkmode=()=>{
+  document.body.classList.remove('dark');
+localStorage.setItem('darkmode',null);
+}
+if(darkmode=="active"){
+  enableDarkmode()}
+  else{  document.body.classList.remove('dark');
+}
+if(themeSwitch){
+themeSwitch.addEventListener("click",() => {
+darkmode=localStorage.getItem('darkmode');
+  if(darkmode!=="active"){
+     enableDarkmode();}
+     else{
+      disableDarkmode();}
+});}
+
+    
+
+
     
     
 //  All products on the page 
