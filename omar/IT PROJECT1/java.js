2f60e535d84ba72a542ab8adb7d925eb1f5d65e6
hanahesham1786-dotcom <hanahesham1786@gmail.com>
2026-05-05 16:32:39 +0300

    Update java.js

--- omar/IT PROJECT1/java.js
+++ omar/IT PROJECT1/java.js
@@ -22,32 +22,6 @@ var swiper = new Swiper(".mySwiper", {
 
 
 
-let darkmode =localStorage.getItem('darkmode');
-const themeSwitch =document.getElementById('theme-switch');
-
-const enableDarkmode = ()=>{
-
-  document.body.classList.add('dark');
-  localStorage.setItem('darkmode','active');
-
-}
-const disableDarkmode=()=>{
-  document.body.classList.remove('dark');
-localStorage.setItem('darkmode',null);
-}
-if(darkmode=="active"){
-  enableDarkmode()}
-  else{  document.body.classList.remove('dark');
-}
-if(themeSwitch){
-themeSwitch.addEventListener("click",() => {
-darkmode=localStorage.getItem('darkmode');
-  if(darkmode!=="active"){
-     enableDarkmode();}
-     else{
-      disableDarkmode();}
-});}
-
     
 
 
